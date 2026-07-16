f3d505c7d3efe8b39b3b662c85ef3e394d481aff
Alcodes <aldisonahadain26@gmail.com>
2026-07-16 21:23:42 +0800

    Fix math equation operators and correct f calculation

--- foundations/04_mathEquations/mathEquations.js
+++ foundations/04_mathEquations/mathEquations.js
@@ -1,9 +1,9 @@
 const a = 1 - 1 // Freebie!!! This is the answer to "one minus one"
 const b = 1 + 8 // This is the answer to "one plus eight"
 const c = 22 * 3 // This is the answer to "twenty-two times three"
-const d = 4 / 4 // This is the answer to "four divided by four"
-const e = 9 - 17 // This is the answer to "nine minus seventeen"
-const f = 68
+const d = 5 % 4 // This is the answer to "four divided by four"
+const e = b - 17 // This is the answer to "nine minus seventeen"
+const f = a + b + c + d + e
 
 // Do not edit below this line
 module.exports = {a, b, c, d, e, f}
