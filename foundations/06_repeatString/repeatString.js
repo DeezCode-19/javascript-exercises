e9781690a9034a28518da57aec11d82354a23571
DeezCode-19 <aldisonahadain26@gmail.com>
2026-08-20 16:10:48 +0800

    Complete repeatString exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+
+let results = "";
+
+    for (let i = 0; i < num; i++) {
+        results += string;
+    }
+
+    return results
 
 };
 
