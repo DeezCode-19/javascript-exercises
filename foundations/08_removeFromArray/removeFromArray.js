089de3424d2826cdfcdfbcdf2a46b87912de3ea6
DeezCode-19 <aldisonahadain26@gmail.com>
2026-08-28 20:16:12 +0800

    Completed the removeFromArray.spec.js exercises

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemsToRemove) {
+  return array.filter(item => !itemsToRemove.includes(item));
 };
 
+module.exports = removeFromArray;
 // Do not edit below this line
 module.exports = removeFromArray;
